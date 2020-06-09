bd95ed5923a23c13b945f9509522e3e2c295a49f
liujiale1999 <2746661234@qq.com>
2020-06-09 22:54:48 +0800

    recharge.html 100%,hash ok

--- js/index.js
+++ js/index.js
@@ -1,22 +1,51 @@
 // 文档加载事件
 $(function () {
-    // 公告垂直方向滚动轮播
-    function autoScroll(obj) {
-        // 每次向上运动20px
-        $(".title ul").animate({
-            marginTop: "-20px"
-        }, 800, function () {
-            $(this).css({ marginTop: "0px" }).find("li:first").appendTo(this);
+    // 通知垂直方向轮播
+    function autoScroll() {
+        $('.title').find('ul').animate({
+            marginTop: '-21px'
+        }, 1000, function () {
+            $(this).css({ marginTop: "0px" });
+            var li = $(".title ul").children().first().clone()
+            $(".title ul li:last").after(li);
+            $(".title ul li:first").remove();
         })
     }
-    $(function () {
-        if ($(".title ul li").length > 2) {
-            setInterval('autoScroll(".title")', 2000);
-        }
-    })
-    // 开启计时器，两秒执行一次
-    setInterval(function () {
-        autoScroll()
-    }, 2000)
+    setInterval(autoScroll, 2000);
 
+    // hash改变时
+    window.onhashchange = loadPages;
+    // 调用一次
+    loadPages()
+    function loadPages() {
+        // 获取hash
+        var hash = location.hash
+        // console.log(hash);
+        switch (hash) {
+            case "":
+                $("#main").load("../../pages/home.html");
+                break;
+            case "#home":
+                $("#main").load("../../pages/home.html");
+                break;
+            case "#pay":
+                $("#main").load("../../pages/pay.html");
+                break;
+            case "#personal":
+                $("#main").load("../../pages/personal.html");
+                break;
+            case "#borrow":
+                $("#main").load("../../pages/borrow.html");
+                break;
+            default :
+            $("#main").load("../../pages/404.html");
+        }
+        // 调用
+        navActive(hash)
+    }
+    // 激活样式
+    function navActive(hash) {
+        // 点击增加当前激活样式，删除其他激活样式
+        $(".main-top .nav-active a[href='" + hash + "']").addClass('active').closest('li').siblings().find('a').removeClass("active")
+    }
 })
